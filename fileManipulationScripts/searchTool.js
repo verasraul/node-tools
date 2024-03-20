b8f34db73bfdc64d25f16dc51c98966dad78421d
Raul Veras <verasraul@gmail.com>
2024-03-20 00:22:35 -0400

    updated functions

--- fileManipulationScripts/searchTool.js
+++ fileManipulationScripts/searchTool.js
@@ -8,15 +8,28 @@ const fs = require('fs');
 function readFile(filename){
     try {
         const data = fs.readFileSync(filename, 'utf8');
-        console.log(data);
+        // console.log(data);
     } catch (err) {
     console.error(err);
     }};
 
-readFile('fileA.txt');
+    
+// readFile('fileA.txt');
+let fileA_content = readFile('fileA.txt');
+let fileB_content = readFile('fileB.txt');
 
-// use function to open file.
-// compare fileA to File. 
-// If values in fileA is in fileB print
 
+// readFile('fileB.txt');
 
+// use function to compare fileA to FileB. 
+// if values in fileA is in fileB print "theses values match."
+// if values in fileA not in fileB print "these values are missing from fileB."
+
+function compareFiles(fileA, fileB){
+    console.log("this is FILE-A:");
+    console.log(`${fileA}`)
+    console.log("this is FILE-B:");
+    console.log(fileB)
+};
+
+compareFiles(fileA_content,fileB_content);
